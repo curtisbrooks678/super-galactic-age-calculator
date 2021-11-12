d6b83ae01106e8daf738edefb723f595b74baf7b
Curtis Brooks <curtisbrooks678@gmail.com>
2021-11-12 10:05:13 -0800

    red phase for first test: should create SpaceHuman object with properties

--- __tests__/spacehuman.test.js
+++ __tests__/spacehuman.test.js
@@ -1,10 +1,10 @@
-import Triangle from '../src/spacehuman.js';
+import SpaceHuman from '../src/spacehuman.js';
 
 describe('Triangle', () => {
 
-//   test('should correctly create a spacehuman object with age and life expectancy properties', () => {
-//     const spacehuman = new SpaceHuman(20, 100);
-//     expect(spacehuman.inputAge).toEqual(20);
-//     expect(spacehuman.lifeExpectancy).toEqual(100);
-//   });
-// });
+  test('should correctly create a spacehuman object with age and life expectancy properties', () => {
+    const spacehuman = new SpaceHuman(20, 100);
+    expect(spacehuman.inputAge).toEqual(20);
+    expect(spacehuman.lifeExpectancy).toEqual(100);
+  });
+});
