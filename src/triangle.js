650a59a472aa8a4198d790006b1c0afced638391
Curtis Brooks <curtisbrooks678@gmail.com>
2021-11-12 09:01:48 -0800

    WIP setting up environment and change class notation

--- src/triangle.js
+++ src/triangle.js
@@ -1,9 +1,11 @@
-export function Triangle(side1, side2, side3) {
-  this.side1 = side1;
-  this.side2 = side2;
-  this.side3 = side3;
-}
-
-Triangle.prototype.checkType = function() {
+export default class Triangle {
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
+  
+  checkType() {
   return "I can't answer that yet!";
+  }
 }
