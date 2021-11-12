807d90ff8593a90df5d9e5da83f1e7b029de0718
Curtis Brooks <curtisbrooks678@gmail.com>
2021-11-12 10:09:52 -0800

    green phase, pass first test, create spacehuman object with properties

--- src/spacehuman.js
+++ src/spacehuman.js
@@ -1,7 +1,6 @@
-export default class Triangle {
-  constructor(side1, side2, side3) {
-    this.side1 = side1;
-    this.side2 = side2;
-    this.side3 = side3;
+export default class SpaceHuman {
+  constructor(inputAge, lifeExpectancy) {
+    this.inputAge = inputAge;
+    this.lifeExpectancy = lifeExpectancy;
   }
 }
